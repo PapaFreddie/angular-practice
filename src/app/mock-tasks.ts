f5668c7ca42ce7b8b96ca753d617e5af8bf10d36
PapaFreddie <fred.papa@student.moringaschool.com>
2022-01-05 13:30:26 +0300

    all files

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import {Task} from './Task'
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Meeting at School',
+        day: 'May 5th at Noon',
+        reminder: true,
+
+    },
+    {
+        id: 2,
+        text: 'Doctors Appointment',
+        day: 'May 6th at Noon',
+        reminder: true,
+
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'May 7th at Noon',
+        reminder: false,
+
+    }
+]
